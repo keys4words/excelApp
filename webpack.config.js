31995a7b16ec8792e6387b9d623cc474f8cba5c4
keys4words <keys4words@gmail.com>
2021-02-24 20:57:37 +0300

    add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,36 @@ module.exports = {
         }),
         new CopyPlugin({
             patterns: [
-              { from: path.resolve(__dirname, 'src/favicon.ico'), 
-              to: path.resolve(__dirname, 'dist') }
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                }
             ],
         }),
         new MiniCssExtractPlugin({
             filename: 'bundle.[hash].css'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
+    },
 }
